90cf483b01797bb42a1d28e7880ae54800daa976
rc-05 <rc-o5@protonmail.com>
2022-07-14 15:30:09 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Anti Fake News Telegram Bot
+
+This bot checks for fake news sites posted on a Telegram chat,
+deletes the message and warns the user who posted said message.
+
+It can be hosted on your machine, provided that you supply your
+own bot token and black list file.
+
+## Build Requirements
+
+- [Haxe](https://haxe.org)
+- [Neko](https://nekovm.com)
+
+## Build Instructions
+
+Simply run either `haxe debug.hxml` or `haxe release.hxml` to create
+a debug or release binary respectively.
+
+Run the __release build__ without arguments to see the command line args.
+
+## Debugging
+
+- Open the project directory in VSCode/VSCodium (make sure that the Haxe and HXCPP debugger extensions are installed).
+- Run the build task `haxe: debug.hxml` or choose the `debug.hxml` configuration and run `haxe: active configuration` task.
+- Run the HXCPP debugger via the graphical menu or with the `F5` key.
+
+The debug build expects to find the `config.json` and `black_list.txt` files
+inside the `extra/` folder.
+
+## Configuration
+
+The bot requires two files to work: a `config.json` which contains the locale and
+the bot token and a `black_list.txt` file which contains all the fake news sites that
+need to be handled.
+
+These two files must be passed to the command line to be read, as said
+earlier, as parameters to cmd line arguments.
+
+### Supported Languages
+
+- English (`en`)
+- Italiano (`it`)
+
+## License
+
+This project is licensed with the [GPL v3.0](https://www.gnu.org/licenses/gpl-3.0.html).
---
+++ src/AntiBufale.hx
@@ -0,0 +1,114 @@
+/*
+	Anti Fake News Telegram Bot
+	Copyright (C) 2022 rc-05
+
+	This program is free software: you can redistribute it and/or modify it under
+	the terms of the GNU General Public License as published by the Free Software Foundation,
+	either version 3 of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along with this program.
+	If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import telegram.types.Core.User;
+import haxe.Log;
+#if !debug
+import hxargs.Args;
+#end
+import haxe.Json;
+import sys.FileSystem;
+import sys.io.File;
+import telegram.Bot;
+import telegram.types.Core.Message;
+
+typedef ConfigFile = {
+	token:String,
+	locale:String
+}
+
+class AntiBufale extends Bot {
+	var config:ConfigFile;
+	var blackList:Array<String>;
+
+	public function new() {
+		#if debug
+		var content = File.getContent("../extra/config.json");
+		config = Json.parse(content);
+		blackList = File.getContent("../extra/black_list.txt").split("\n");
+		#else
+		parseCmdLine();
+		#end
+
+		super(config.token);
+	}
+
+	#if !debug
+	function parseCmdLine() {
+		var cmdArgsHandler = Args.generate([@doc("JSON config file path")
+			["-json"] => function(arg:String) {
+				if (FileSystem.exists(arg)) {
+					var content = File.getContent(arg);
+					config = Json.parse(content);
+				}
+			}, @doc("Black list file path")
+			["-blacklist"] => function(arg:String) {
+				if (FileSystem.exists(arg)) {
+					blackList = File.getContent(arg).split("\n");
+				}
+			}
+		]);
+
+		var cmdArgs = Sys.args();
+		if (cmdArgs.length == 0) {
+			Sys.println(cmdArgsHandler.getDoc());
+			Sys.exit(0);
+		} else {
+			cmdArgsHandler.parse(cmdArgs);
+		}
+	}
+	#end
+
+	override public function onMessageEvent(message:Message) {
+		// Taken from https://ihateregex.io/expr/url/
+		final urlRegex = ~/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
+
+		trace('${message.chat.id}');
+
+		if (message.chat.type == "group" || message.chat.type == "supergroup") {
+			if (urlRegex.match(message.text)) {
+				searchFakeNews(message, urlRegex.matched(0));
+			}
+		}
+	}
+
+	function searchFakeNews(message:Message, url:String) {
+		for (entryUrl in blackList) {
+			if (entryUrl == url) {
+				deleteMessage(message.chat.id, message.message_id);
+				switch (config.locale) {
+					case "en":
+						sendMessage(message.chat.id, 'Fake news site detected, don\'t try it anymore @${message.from.username}');
+					case "it":
+						sendMessage(message.chat.id, 'Sito di bufale individuato, non ci provare più @${message.from.username}');
+					default:
+						// Fall back to english locale if not explicitly set.
+						sendMessage(message.chat.id, 'Fake news site detected, don\'t try it anymore @${message.from.username}');
+				}
+				logFoundSite(url, message.from);
+			}
+		}
+	}
+
+	function logFoundSite(url:String, from:User) {
+		Sys.println('[${Date.now().toString()}] Found fake news site $url posted by @${from.username}');
+	}
+
+	static function main() {
+		var bot = new AntiBufale();
+		bot.start();
+	}
+}
